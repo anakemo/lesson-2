5a2a14ffa62880bd2b3521c2cae9d21302592902
anakemo <anakemoklidze1993@gmail.com>
2022-10-04 00:25:08 +0200

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,70 @@
+// #2
+
+
+// function sum(...numbers){
+//     let numberSum=0;
+//     for (items of numbers){
+//         numberSum=numberSum+ items;
+//     }
+//     return numberSum;
+
+// }
+
+
+// let result=sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(result);
+
+
+// #3
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   }
+  
+// function show(){
+//     if(user.isloggedin==true){
+//         return(user.firstname +" "+user.lastname);
+//     }else{
+//         return(false);
+//     }
+// }
+// console.log(show());
+
+// #4
+
+
+//  function showmax(number){
+//     let maxValue=0;
+//    for(let item of number){
+//     if(item > maxValue) {
+//         maxValue=item;
+//     }
+//    }
+//    return(maxValue);
+//  }
+
+
+//  let result=showmax([10, 20, 5, 100, 300,3, 9, 15]);
+//  console.log(result);
+
+// #5
+
+// let array = [1,2,4,10,34,5,7,87];
+// for(item of array){
+//     if(item > 0  && item< 10 ){
+//         console.log(item);
+//     }
+// }
+
+// #6
+//  let numbers =[1,2,3,4,5,6,7,8,9,10];
+// for(let item of numbers){
+//     console.log(item);
+//     if(item==5) {
+//         break
+        
+//     }
+// }
